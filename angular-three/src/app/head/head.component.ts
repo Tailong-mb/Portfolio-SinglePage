2bb0f50a5b283573109c6ab99ca49a288f8ad3a0
Michael Bardou <michael.bardou@icloud.com>
2022-06-24 15:31:25 +0200

    continued head component

--- angular-three/src/app/head/head.component.ts
+++ angular-three/src/app/head/head.component.ts
@@ -1,15 +1,33 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
-  styleUrls: ['./head.component.css']
+  styleUrls: ['./head.component.css'],
+  animations: [
+    trigger('descriptionAnimation', [
+      transition('void => *', [
+        query('p', style({ transform: 'translateX(40px)' })),
+        query(
+          'p',
+          stagger('400ms', [
+            animate('500ms 1s ease-out', style({ transform: 'translateX(0)' })),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HeadComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
