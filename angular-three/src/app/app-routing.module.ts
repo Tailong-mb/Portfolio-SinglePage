c35b53a6932f4728fc485a096ba65eb284a69756
Michael Bardou <michael.bardou@icloud.com>
2022-06-24 10:56:32 +0200

    modfication on routing

--- angular-three/src/app/app-routing.module.ts
+++ angular-three/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeadComponent } from './head/head.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'CV-Yanis', pathMatch: 'full' },
+  { path: 'CV-Yanis', component: HeadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
