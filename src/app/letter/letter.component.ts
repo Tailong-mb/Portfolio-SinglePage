02a1ed1835d34e08c0a1081e276f8074f43e2e9b
Michael Bardou <michael.bardou@icloud.com>
2022-08-05 18:01:21 +0200

    start letter component

--- src/app/letter/letter.component.ts
+++ src/app/letter/letter.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-letter',
   templateUrl: './letter.component.html',
-  styleUrls: ['./letter.component.css']
+  styleUrls: ['./letter.component.css'],
 })
-export class LetterComponent implements OnInit {
+export class LetterComponent {
+  @Input() word!: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
