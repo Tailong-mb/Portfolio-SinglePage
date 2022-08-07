1ba9b02329a53fba6246abba4d7d4d2fa4073a6e
Michael Bardou <michael.bardou@icloud.com>
2022-08-07 18:15:56 +0200

    fadeIn animation global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+// Animation fadeIn
+export const fadeIn = trigger('fadeIn', [
+  state(
+    'void',
+    style({
+      opacity: 0,
+      transform: 'translateY(20px)',
+    })
+  ),
+  transition(':enter', [
+    animate(
+      '.5s ease-in',
+      style({
+        opacity: 1,
+        transform: 'translateY(0)',
+      })
+    ),
+  ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [fadeIn],
 })
 export class AppComponent {
   title = 'YANIS';
--- src/app/letter/letter.component.ts
+++ src/app/letter/letter.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { fadeIn } from '../app.component';
 
 @Component({
   selector: 'app-letter',
   templateUrl: './letter.component.html',
   styleUrls: ['./letter.component.css'],
+  animations: [fadeIn],
 })
 export class LetterComponent {
   @Input() word!: String;
