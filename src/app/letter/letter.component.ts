26e9404a3256fc70d7eddcda8d6da52790505b53
Michael Bardou <michael.bardou@icloud.com>
2022-08-07 12:31:41 +0200

    correct issue (animation) letter component

--- src/app/letter/letter.component.ts
+++ src/app/letter/letter.component.ts
@@ -1,12 +1,4 @@
 import { Component, HostListener, Input } from '@angular/core';
-import {
-  animate,
-  query,
-  stagger,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 
 @Component({
   selector: 'app-letter',
@@ -18,16 +10,21 @@ export class LetterComponent {
 
   inComponent = false;
 
-  @HostListener('mouseenter')
-  onMouseEnter() {
-    Array.from(document.getElementsByClassName('letter')).forEach((letter) => {
+  @HostListener('mouseenter', ['$event.target'])
+  onMouseEnter(event: any) {
+    const letters = event.target.getElementsByClassName('letter');
+    Array.from(letters).forEach((letter) => {
+      // @ts-ignore
       letter.classList.add('animate');
     });
   }
 
-  @HostListener('mouseleave')
-  onMouseLeave() {
-    Array.from(document.getElementsByClassName('letter')).forEach((letter) => {
+  @HostListener('mouseleave', ['$event.target'])
+  onMouseLeave(event: any) {
+    const element = event.target;
+    const letters = element.getElementsByClassName('letter');
+    Array.from(letters).forEach((letter) => {
+      // @ts-ignore
       letter.classList.remove('animate');
     });
   }
