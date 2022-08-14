3c83b85818bf16a057b2ca31ee33fa7f1dd9bf2d
Michael Bardou <michael.bardou@icloud.com>
2022-08-14 19:59:57 +0200

    save

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,29 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {fadeIn, fadeOut} from "../app.component";
+import { Component, Input, OnInit } from '@angular/core';
+import { fadeIn, fadeOut } from '../app.component';
+import { experiencesData } from './experiencesData';
 
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
   styleUrls: ['./experiences.component.css'],
-  animations:[fadeIn, fadeOut],
+  animations: [fadeIn, fadeOut],
 })
 export class ExperiencesComponent implements OnInit {
-  collapsed:boolean = false;
-  crossStatue:string = 'cross-open';
-  @Input() mode!:string;
+  collapsed: boolean = false;
+  crossStatue: string = 'cross-open';
+  experienceData = experiencesData;
+
+  @Input() mode!: string;
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
-    this.crossStatue = this.crossStatue === 'cross-open' ? 'cross-close' : 'cross-open';
+    this.crossStatue =
+      this.crossStatue === 'cross-open' ? 'cross-close' : 'cross-open';
   }
 
   getCrossStatue(): string {
-     return this.mode === 'light' ? this.crossStatue + ' cross-light' : this.crossStatue + ' cross-dark';
-    }
-
-  constructor() { }
-
-  ngOnInit(): void {
+    return this.mode === 'light'
+      ? this.crossStatue + ' cross-light'
+      : this.crossStatue + ' cross-dark';
   }
 
+  getLine(): string {
+    return this.mode === 'light' ? 'line-light' : 'line-dark';
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
---
+++ src/app/experiences/experiencesData.ts
@@ -0,0 +1,8 @@
+export const experiencesData = [
+  {
+    titre: 'Atas',
+    image: 'assets/images/atas.jpg',
+    lien: 'https://drive.google.com/file/d/1qHmaw3ythMfFhF3bCARlxYSIAYxsHGVn/view?usp=sharing',
+    description: 'test',
+  },
+];
