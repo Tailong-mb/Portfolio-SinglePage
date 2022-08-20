f9e708491c475824a100af066c4fba4fc84fb385
Michael Bardou <michael.bardou@icloud.com>
2022-08-20 20:06:59 +0200

    start education component"

---
+++ src/app/education/education-data.ts
@@ -0,0 +1,23 @@
+export const educationData = [
+  {
+    title: 'Bachelor of Science in Computer Science',
+    subtitle: 'University of Waterloo',
+    date: 'Sep. 2019 - May. 2020',
+    description:
+      'I graduated with a 3.8 GPA and a distinction in the coursework. I undertook a variety of courses including: Software Engineering, Data Structures, Algorithms, and Object-Oriented Programming. I also undertook a variety of electives including: Web Development, Database Systems, and Computer Networks.',
+  },
+  {
+    title: 'Bachelor of Science in Computer Science',
+    subtitle: 'University of Waterloo',
+    date: 'Sep. 2019 - May. 2020',
+    description:
+      'I graduated with a 3.8 GPA and a distinction in the coursework. I undertook a variety of courses including: Software Engineering, Data Structures, Algorithms, and Object-Oriented Programming. I also undertook a variety of electives including: Web Development, Database Systems, and Computer Networks.',
+  },
+  {
+    title: 'Bachelor of Science in Computer Science',
+    subtitle: 'University of Waterloo',
+    date: 'Sep. 2019 - May. 2020',
+    description:
+      'I graduated with a 3.8 GPA and a distinction in the coursework. I undertook a variety of courses including: Software Engineering, Data Structures, Algorithms, and Object-Oriented Programming. I also undertook a variety of electives including: Web Development, Database Systems, and Computer Networks.',
+  },
+];
--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { educationData } from './education-data';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+  styleUrls: ['./education.component.css', './../app.component.css'],
 })
 export class EducationComponent implements OnInit {
+  educationData = educationData;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
